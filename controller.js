ba065e43aad3460aeb9eb9b489ea8aa2eb5e16dc
Liya Dong <2710780954@qq.com>
2017-05-16 10:12:41 +0800

    Delete controller.js

--- controller.js
+++
@@ -1,9 +0,0 @@
-function AppCtrl($scope, $http) {
-	console.log("Hello world from controller");
-	$http.get('/contactList').success(function(response){
-		console.log("I got the data I requested");
-		$scope.contactList = response;
-	});
-	
-	
-}
